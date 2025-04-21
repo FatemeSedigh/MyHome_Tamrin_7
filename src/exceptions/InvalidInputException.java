b7ba67067c3451ac49203745d82b04113e142205
fati <fatemesadatsedigh@gmail.com>
2025-04-21 18:46:43 +0330

    add InvalidInputException

--- src/exceptions/InvalidInputException.java
+++ src/exceptions/InvalidInputException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class InvalidInputException {
+class InvalidInputException extends Exception {
+    public InvalidInputException(String message) { super(message); }
+    public static InvalidInputException general() { return new InvalidInputException("invalid input"); }
+    public static InvalidInputException forCommand(String command) { return new InvalidInputException("invalid " + command + " command"); }
 }
