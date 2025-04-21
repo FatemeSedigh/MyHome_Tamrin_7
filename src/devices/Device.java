08b1cba07fcd102decff7c6c3aa5b5d77b89a852
fati <fatemesadatsedigh@gmail.com>
2025-04-21 14:50:32 +0330

    add public Device

--- src/devices/Device.java
+++ src/devices/Device.java
@@ -6,5 +6,12 @@ public abstract class Device {
     protected String protocol;
     protected String status;
 
+    public Device(String name, String protocol, String status){
+        this.name = name;
+        this.protocol = protocol;
+        this.status = "off";
+    }
+
+
 
 }
