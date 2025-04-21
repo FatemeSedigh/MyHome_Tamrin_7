4b66e1e672e6e731d857f6ed403c484051babd71
fati <fatemesadatsedigh@gmail.com>
2025-04-21 15:03:22 +0330

    add public Light

--- src/devices/Light.java
+++ src/devices/Light.java
@@ -4,6 +4,11 @@ public class Light extends Device{
 
     private int brightness;
 
+    public Light(String name, String protocol){
+        super(name, protocol);
+        this.brightness = 50;
+    }
+
 
 
 }
