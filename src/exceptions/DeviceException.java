03d3c89873b22b5adcf4444f384107199ab6babd
fati <fatemesadatsedigh@gmail.com>
2025-04-21 18:46:16 +0330

    add DeviceException

--- src/exceptions/DeviceException.java
+++ src/exceptions/DeviceException.java
@@ -1,4 +1,9 @@
 package exceptions;
 
-public class DeviceException {
+class DeviceException extends Exception {
+    public DeviceException(String message) { super(message); }
+    public static DeviceException duplicateDeviceName() { return new DeviceException("duplicate device name"); }
+    public static DeviceException deviceNotFound() { return new DeviceException("device not found"); }
+    public static DeviceException invalidProperty() { return new DeviceException("invalid property"); }
+    public static DeviceException invalidValue() { return new DeviceException("invalid value"); }
 }
