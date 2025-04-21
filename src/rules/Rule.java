3eb96e4c4af9ef51597f8a80eba1e36d9a381e6c
fati <fatemesadatsedigh@gmail.com>
2025-04-21 18:28:02 +0330

    add public Rule

--- src/rules/Rule.java
+++ src/rules/Rule.java
@@ -6,6 +6,12 @@ public class Rule {
     private String time;
     private String action;
 
+    public Rule(String deviceName,String time, String action){
+        this.deviceName = deviceName;
+        this.time = time;
+        this.action = action;
+    }
+
 
 
 }
