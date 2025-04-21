9d4a7edf7325613f9dd6f9ae7ec4135d66229ede
fati <fatemesadatsedigh@gmail.com>
2025-04-21 14:54:36 +0330

    add getter and setter

--- src/devices/Device.java
+++ src/devices/Device.java
@@ -12,6 +12,10 @@ public abstract class Device {
         this.status = "off";
     }
 
+    public String getName(){ return name; }
+    public String getStatus(){ return status; }
+    public String getProtocol(){ return protocol; }
 
+    public void setStatus(String status){ this.status = status; }
 
 }
