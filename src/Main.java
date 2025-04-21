180fbb4adc7b335a4f764b736bff49a0c6d1c1cf
fati <fatemesadatsedigh@gmail.com>
2025-04-21 18:58:04 +0330

    add fields

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import devices.Device;
+import rules.Rule;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+
+    private static Map<String, Device> devices = new LinkedHashMap<>();
+    private static List<Rule> rules = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+
     }
 }
